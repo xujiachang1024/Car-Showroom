7d09a3286c9205d10098bb72019e9080434526c0
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-08-30 20:18:39 +0100

    Implement the Car class

---
+++ car.py
@@ -0,0 +1,24 @@
+class Car(object):
+
+    def __init__(self, make, year, price):
+        self.__make = make
+        self.__year = year
+        self.__price = price
+
+    def setMake(self, make):
+        self.__make = make
+
+    def getMake(self):
+        return self.__make
+
+    def setYear(self, year):
+        self.__year = year
+
+    def getYear(self):
+        return self.__year
+
+    def setPrice(self, price):
+        self.__price = price
+
+    def getPrice(self):
+        return self.__price
